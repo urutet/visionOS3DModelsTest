45aee03718d73ff35dc7a91fd50b5115e26e9861
Ilya Yushkevich <ilya.yushkevich@leverx.com>
2024-05-21 15:59:43 +0200

    Removed unused properties

--- visionOS3DModelsTest/Item/ItemView.swift
+++ visionOS3DModelsTest/Item/ItemView.swift
@@ -16,16 +16,6 @@ private enum Constants {
 struct ItemView: View {
     @State var viewModel: ItemViewModel
     
-    // Rotations
-    
-    // - TODO: Refactor to incapsulate these properties in component
-    // `https://developer.apple.com/documentation/realitykit/transforming-realitykit-entities-with-gestures?changes=_8`
-    @State var angle: Angle = .degrees(0)
-    @State var startAngle: Angle?
-    @State var axis: (CGFloat, CGFloat, CGFloat) = (.zero, .zero, .zero)
-    @State var startAxis: (CGFloat, CGFloat, CGFloat)?
-    @State var scale: Double = 1
-    @State var startScale: Double?
     @State var entity: Entity? {
         didSet {
             guard let entity else { return }
@@ -109,8 +99,7 @@ struct ItemView: View {
                             selectedEntity = value.entity.clone(recursive: true)
                             guard let entity else { return }
                             // Sets the scale of the root entity to the selected entity
-                            selectedEntity?.transform.scale = entity.transform.scale
-                            selectedEntity?.transform.rotation = entity.transform.rotation
+                            selectedEntity?.transform = entity.transform
                         }
                 )
             }
